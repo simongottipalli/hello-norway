143ad2dd8931b09e3fabf2eb6f532128ba12cc0e
simongottipalli <simon.gottipalli@gmail.com>
2026-03-03 16:24:48 +0100

    fix more warnings

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
   serverExternalPackages: ['pino', 'pino-pretty'],
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
