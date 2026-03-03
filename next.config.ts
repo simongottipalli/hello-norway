f033543786ba2b9df9a64c426aa920ce30e44b96
simongottipalli <simon.gottipalli@gmail.com>
2026-03-03 16:32:49 +0100

    simpler

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
   serverExternalPackages: ['pino', 'pino-pretty'],
   turbopack: {
-    root: path.resolve(__dirname),
+    root: __dirname,
   },
 };
 
